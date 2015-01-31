799a036c984525c7736fd2de381b47480c1dccbb
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-01-31 20:00:15 +0530

    service creation

---
+++ SocioWebService/IregAuth.vb
@@ -0,0 +1,10 @@
+﻿Imports System.ServiceModel
+
+' NOTE: You can use the "Rename" command on the context menu to change the interface name "IregAuth" in both code and config file together.
+<ServiceContract()>
+Public Interface IregAuth
+
+    <OperationContract()>
+    Sub DoWork()
+
+End Interface
---
+++ SocioWebService/regAuth.svc.vb
@@ -0,0 +1,9 @@
+﻿' NOTE: You can use the "Rename" command on the context menu to change the class name "regAuth" in code, svc and config file together.
+' NOTE: In order to launch WCF Test Client for testing this service, please select regAuth.svc or regAuth.svc.vb at the Solution Explorer and start debugging.
+Public Class regAuth
+    Implements IregAuth
+
+    Public Sub DoWork() Implements IregAuth.DoWork
+    End Sub
+
+End Class
