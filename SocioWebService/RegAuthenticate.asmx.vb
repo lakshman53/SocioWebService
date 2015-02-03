d17bc59ec24497e12c6cb162d26507474329bf7d
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-02-03 22:56:35 +0530

    after WS change

--- SocioWebService/RegAuthenticate.asmx.vb
+++ SocioWebService/RegAuthenticate.asmx.vb
@@ -18,7 +18,7 @@ Public Class RegAuthenticate
 
     End Function
     <WebMethod()> _
-    Public Function isEmployeeExists(ByVal EmpId As String, ByVal MobileNo As String, ByVal Email As String) As Boolean
+    Public Function isEmployeeExists(ByVal EmpId As String, ByVal MobileNo As String, ByVal Email As String) As Integer
         Dim connectionString As String = _
             "Server=tcp:mqjkakgxkk.database.windows.net,1433;Database=sociodb;" & _
                                     "User ID=laks@mqjkakgxkk;Password=sqlPassword@123;" & _
@@ -31,7 +31,7 @@ Public Class RegAuthenticate
             & "AND Email = @Email " _
             & "AND MobileNo = @MobileNo"
 
-        
+
         Using connection As New SqlConnection(connectionString)
 
             Dim command As New SqlCommand(queryString, connection)
@@ -40,7 +40,6 @@ Public Class RegAuthenticate
             command.Parameters.AddWithValue("@Email", Email)
             command.Parameters.AddWithValue("@MobileNo", MobileNo)
 
-
             Try
                 connection.Open()
                 Dim dataReader As SqlDataReader = _
@@ -48,9 +47,10 @@ Public Class RegAuthenticate
 
                 Do While dataReader.Read()
                     If dataReader(0) = 1 Then
-                        Return True
+
+                        Return Int(Rnd() * 1000)
                     Else
-                        Return False
+                        Return -1
                     End If
                 Loop
                 dataReader.Close()
@@ -64,5 +64,56 @@ Public Class RegAuthenticate
         Return True
 
     End Function
+
+    '<WebMethod()> _
+    'Public Function logAttendance(ByVal EmpId As String, ByVal LoginDate As Date, ByVal LogDateTime As Date, ByVal LogFlag As Integer, ByVal ) As Boolean
+
+    'Dim connectionString As String = _
+    '"Server=tcp:mqjkakgxkk.database.windows.net,1433;Database=sociodb;" & _
+    '"User ID=laks@mqjkakgxkk;Password=sqlPassword@123;" & _
+    '"Trusted_Connection=False;Encrypt=True;Connection Timeout=30;"
+
+
+
+    ' Provide the query string with a parameter placeholder. 
+    ' Dim queryString As String = _
+    '    "SELECT count(1) from dbo.vEmployee " _
+    '    & "WHERE EmpId = @EmpId " _
+    '    & "AND Email = @Email " _
+    '    & "AND MobileNo = @MobileNo"
+
+
+    'Using connection As New SqlConnection(connectionString)
+
+    '    Dim command As New SqlCommand(queryString, connection)
+
+    '    command.Parameters.AddWithValue("@EmpId", EmpId)
+    '    command.Parameters.AddWithValue("@Email", Email)
+    '    command.Parameters.AddWithValue("@MobileNo", MobileNo)
+
+
+    '    Try
+    '        connection.Open()
+    '        Dim dataReader As SqlDataReader = _
+    '         command.ExecuteReader()
+
+    '        Do While dataReader.Read()
+    '            If dataReader(0) = 1 Then
+    '                Return True
+    '            Else
+    '                Return False
+    '            End If
+    '        Loop
+    '        dataReader.Close()
+
+    '    Catch ex As Exception
+    '    Console.WriteLine(ex.Message)
+    'End Try
+    'Console.ReadLine()
+    'End Using
+
+    'Return True
+
+    'End Function
 
 End Class
