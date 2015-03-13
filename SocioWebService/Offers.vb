614552ae82496e1d3edc2a11acf85ac25b547892
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-03-13 22:42:29 +0530

    new

--- SocioWebService/Offers.vb
+++ SocioWebService/Offers.vb
@@ -6,13 +6,15 @@
 Public Class Offers
 
     'Public Subject, Offer, Sent As String
-    Public datatable As New DataTable
+    Public offerString As String
 
     Public Sub New(ByVal EmpId As Integer, ByVal LastOfferId As Integer)
         Dim selectStatement As String
         Dim connectionString As String = System.Configuration.ConfigurationManager.AppSettings("ConnectionString").ToString()
-
-        selectStatement = "SELECT  Offer.subject, Offer.message, convert(nvarchar(10),Offer.sent,105) + ' ' + convert(nvarchar(5),Offer.sent,108) [SentAt] FROM OfferGroups INNER JOIN Offer ON OfferGroups.OfferId = Offer.OfferId INNER JOIN"
+        Dim datatable As New DataTable
+        Dim dataset As New DataSet
+        ', convert(nvarchar(10),Offer.sent,105) + ' ' + convert(nvarchar(5),Offer.sent,108) [SentAt]
+        selectStatement = "SELECT  Offer.OfferId, Offer.subject, Offer.message, Offer.sent [SentAt] FROM OfferGroups INNER JOIN Offer ON OfferGroups.OfferId = Offer.OfferId INNER JOIN"
         selectStatement = selectStatement + " [Group] ON OfferGroups.GroupId = [Group].GroupId INNER JOIN"
         selectStatement = selectStatement + " EmpGroups ON [Group].GroupId = EmpGroups.GroupId INNER JOIN"
         selectStatement = selectStatement + " Employee ON EmpGroups.EmpId = Employee.EmpId"
@@ -26,12 +28,15 @@ Public Class Offers
             Dim dataReader As SqlDataReader = command.ExecuteReader()
 
             Dim colCnt As Integer = dataReader.FieldCount
+            datatable.Columns.Add("OfferId")
             datatable.Columns.Add("Subject")
             datatable.Columns.Add("Offer")
             datatable.Columns.Add("SentAt")
             datatable.TableName = "Offers"
             If dataReader.HasRows Then
                 datatable.Load(dataReader)
+                dataset.Tables.Add(datatable)
+                offerString = dataset.GetXml()
             Else
 
             End If
